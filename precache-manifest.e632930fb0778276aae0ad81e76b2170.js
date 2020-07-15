d4bbcde697354d62728ec81c58893ed4c03157fe
anhtranhoang <hoanganhvt12@gmail.com>
2020-07-15 17:40:44 +0700

    Updates

---
+++ precache-manifest.e632930fb0778276aae0ad81e76b2170.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8aab1bc443dc393729081a16900d135",
+    "url": "/webfood/index.html"
+  },
+  {
+    "revision": "b7d1a533a9120c6a6495",
+    "url": "/webfood/static/css/main.1b20477e.chunk.css"
+  },
+  {
+    "revision": "e3e64040f52cd4d84fd6",
+    "url": "/webfood/static/js/2.a803a860.chunk.js"
+  },
+  {
+    "revision": "5fd2cb336184a2018f7da3e6a939bc3a",
+    "url": "/webfood/static/js/2.a803a860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d1a533a9120c6a6495",
+    "url": "/webfood/static/js/main.524c87a9.chunk.js"
+  },
+  {
+    "revision": "cd52c84b8ead06430a49",
+    "url": "/webfood/static/js/runtime-main.20a2a525.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webfood/static/media/logo.5d5d9eef.svg"
+  }
+]);
